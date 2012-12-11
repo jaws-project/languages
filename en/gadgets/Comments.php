8d9e24edc928c000433edab071017f000ad87182
Ali Fazelzadeh <afz@dev-code.com>
2012-12-12 02:12:34 +0330

    Added Comments translate file

---
+++ en/gadgets/Comments.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: Chatbox"
+ * "Last-Translator: "
+ * "Language-Team: EN"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_EN_COMMENTS_NAME', "Comments");
+define('_EN_COMMENTS_DESCRIPTION', "A gadget to mangament comments");
