0ccb028b38c5eab5756780a49a82ec074e399fc8
Ali Fazelzadeh <afz@dev-code.com>
2013-02-10 17:52:03 +0330

    [ServerTime]: Updated layout actions names/descriptions

--- en/gadgets/ServerTime.php
+++ en/gadgets/ServerTime.php
@@ -13,6 +13,10 @@
 define('_EN_SERVERTIME_NAME', "ServerTime");
 define('_EN_SERVERTIME_DESCRIPTION', "A gadget to show the time of your server with any format");
 
+/* Actions */
+define('_EN_SERVERTIME_ACTIONS_DISPLAY', "Display server time");
+define('_EN_SERVERTIME_ACTIONS_DISPLAY_DESC', "Display server time with any format");
+
 /* Common strings */
 define('_EN_SERVERTIME_ACTION_TITLE', "Time on server");
 define('_EN_SERVERTIME_FORMAT_TEXT', "Format Date & Time for Display");
@@ -21,10 +25,6 @@ define('_EN_SERVERTIME_EXAMPLE_TEXT', "ex: Today is [t]DN[/t], [t]d[/t] [t]MN[/t
 /* ALCs */
 define('_EN_SERVERTIME_ACL_DEFAULT', "Manage Server time");
 define('_EN_SERVERTIME_ACL_UPDATEPROPERTIES', "Update Properties");
-
-/* Layout */
-define('_EN_SERVERTIME_LAYOUT_DISPLAY', "Display server time");
-define('_EN_SERVERTIME_LAYOUT_DISPLAY_DESC', "Display server time with any format");
 
 /* Responses */
 define('_EN_SERVERTIME_PROPERTIES_UPDATED', "The properties have been updated");
