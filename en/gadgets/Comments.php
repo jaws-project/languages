9fb5f4036fffdaa4b62d4cd9864d67d5e3919bcd
Mojtaba Ebrahimi <irangame@gmail.com>
2013-02-21 18:43:18 +0330

    [comments]: Added missing translations

--- en/gadgets/Comments.php
+++ en/gadgets/Comments.php
@@ -15,3 +15,15 @@ define('_EN_COMMENTS_DESCRIPTION', "A gadget to mangament comments");
 
 define('_EN_COMMENTS_ACTIONS_RECENTCOMMENTS', "Recent comments");
 define('_EN_COMMENTS_ACTIONS_RECENTCOMMENTS_DESC', "show list of recent comments");
+
+define('_EN_COMMENTS_GADGETS', "Gadget");
+define('_EN_COMMENTS_FILTER', "Filter");
+define('_EN_COMMENTS_EDIT_MESSAGE_DETAILS', "Edit comment");
+define('_EN_COMMENTS_MESSAGE', "Message");
+
+define('_EN_COMMENTS_SEARCH_POST_ID_IS', "Post Id");
+define('_EN_COMMENTS_SEARCH_COMMENT_CONTAINS', "Comment");
+define('_EN_COMMENTS_SEARCH_NAME_CONTAINS', "Name");
+define('_EN_COMMENTS_SEARCH_EMAIL_CONTAINS', "Email");
+define('_EN_COMMENTS_SEARCH_URL_CONTAINS', "URL");
+define('_EN_COMMENTS_SEARCH_IP_IS', "IP");
