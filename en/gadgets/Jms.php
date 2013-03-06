546e246e2b2f7852a679f82ae53e1c2729bdcbcf
Mohsen Khahani <mkhahani@gmail.com>
2013-03-06 20:33:37 +0330

    update JMS en translation

--- en/gadgets/Jms.php
+++ en/gadgets/Jms.php
@@ -21,61 +21,30 @@ define('_EN_JMS_DESCRIPTION', "Manage gadgets and plugins.");
 define('_EN_JMS_UPDATE', "Update");
 define('_EN_JMS_INSTALL', "Install");
 define('_EN_JMS_UNINSTALL', "Uninstall");
-define('_EN_JMS_ENABLE', "Enable");
 define('_EN_JMS_DISABLE', "Disable");
 define('_EN_JMS_VERSION', "Version");
 define('_EN_JMS_JAWS_VERSION', "Required Jaws version");
 define('_EN_JMS_DEPENDENCIES', "Dependencies");
-
-define('_EN_JMS_GADGET_LOCATION', "Location");
-
-define('_EN_JMS_KEY', "Key");
-define('_EN_JMS_VALUE', "Value");
 
 /* ACL */
 define('_EN_JMS_ACL_MANAGEJMS', "Use component manager");
 define('_EN_JMS_ACL_MANAGEGADGETS', "Enable/Disable gadgets");
 define('_EN_JMS_ACL_MANAGEPLUGINS', "Enable/Disable plugins");
 
-/* Plugins */
-define('_EN_JMS_PLUGINS', "Plugins");
-define('_EN_JMS_PLUGINS_INSTALLED_PLUGINS', "Installed plugins");
-define('_EN_JMS_PLUGINS_NOTINSTALLED_PLUGINS', "Not installed plugins");
-
-define('_EN_JMS_PLUGINS_PLUGIN', "Plugin");
-define('_EN_JMS_PLUGINS_USAGE', "Plugin Usage");
-define('_EN_JMS_PLUGINS_NO_EXAMPLE', "No example found");
-define('_EN_JMS_PLUGINS_FRIENDLY', "Friendly");
-define('_EN_JMS_PLUGINS_NOT_FRIENDLY', "Not friendly");
-define('_EN_JMS_PLUGINS_ACCESSKEY', "Access Key");
-define('_EN_JMS_PLUGINS_NO_ACCESSKEY', "No access key found");
-define('_EN_JMS_PLUGINS_USE_IN', "Plugin usage");
-define('_EN_JMS_PLUGINS_SAVED', "Your plugin settings have been saved.");
-define('_EN_JMS_PLUGINS_NOTHING', "No plugins");
-define('_EN_JMS_PLUGINS_ALWAYS', "Always");
-define('_EN_JMS_PLUGINS_ONLY_IN_GADGET', "Only in these pluggable gadgets");
-define('_EN_JMS_PLUGINS_ENABLED_OK', "The {0} plugin has been installed sucessfully");
-define('_EN_JMS_PLUGINS_ENABLED_FAILURE', "There was a problem installing {0} plugin");
-define('_EN_JMS_PLUGINS_NO_SELECTION', "No plugin has been selected, please select one");
-define('_EN_JMS_SIDEBAR_DISABLE_PLUGIN', "Disable");
-define('_EN_JMS_SIDEBAR_DISABLE_PLUGINS', "Enabled Plugins");
-define('_EN_JMS_SIDEBAR_DISABLE_PLUGINS_WAITING', "Plugin waiting to be disabled");
-define('_EN_JMS_PLUGINS_DISABLE_FAILURE', "Could not disable plugin");
-define('_EN_JMS_PLUGINS_DISABLE_OK', "Plugin uninstalled sucessfully");
-define('_EN_JMS_PLUGINS_PLUGIN_DOESNT_EXISTS', "The plugin {0} does not exists");
-define('_EN_JMS_PLUGINS_PLUGIN_CANT_HAVE_NAME_CORE', "The plugin can't have the name Core, it's reserved for Jaws.");
-define('_EN_JMS_PLUGINS_UNINSTALL_CONFIRM', "Do you want to uninstall this plugin ?");
+/* GLOBAL (is used on entire jaws) */
+define('_EN_JMS_SIDEBAR_DISABLE_MAIN_FAILURE', "Cannot disable main gadget");
 
 /* Gadgets */
 define('_EN_JMS_GADGETS', "Gadgets");
-define('_EN_JMS_GADGETS_INSTALLED_GADGETS', "Installed gadgets");
-define('_EN_JMS_GADGETS_INSTALLED_GADGETS_DESC', "There are gadgets you can uninstall.");
-define('_EN_JMS_GADGETS_OUTDATED_GADGETS', "Outdated gadgets");
-define('_EN_JMS_GADGETS_OUTDATED_GADGETS_DESC', "These are gadgets that can be updated now.");
-define('_EN_JMS_GADGETS_NOTINSTALLED_GADGETS', "Not installed gadgets");
-define('_EN_JMS_GADGETS_NOTINSTALLED_GADGETS_DESC', "There are gadgets waiting to be installed.");
-define('_EN_JMS_GADGETS_CONFIRM_DISABLE', "Do you want to disable the gadget? This does not delete data from database.");
+define('_EN_JMS_GADGETS_INSTALLED', "Installed gadgets");
+define('_EN_JMS_GADGETS_INSTALLED_DESC', "There are gadgets you can uninstall.");
+define('_EN_JMS_GADGETS_OUTDATED', "Outdated gadgets");
+define('_EN_JMS_GADGETS_OUTDATED_DESC', "These are gadgets that can be updated now.");
+define('_EN_JMS_GADGETS_NOTINSTALLED', "Not installed gadgets");
+define('_EN_JMS_GADGETS_NOTINSTALLED_DESC', "There are gadgets waiting to be installed.");
+define('_EN_JMS_GADGETS_LOCATION', "Location");
 define('_EN_JMS_GADGETS_CONFIRM_UNINSTALL', "Do you want to uninstall the gadget? This will delete data from database.");
+define('_EN_JMS_GADGETS_CONFIRM_DISABLE', "Do you want to disable the gadget? This does not delete data from database.");
 
 define('_EN_JMS_GADGETS_NOTHING', "No gadgets");
 define('_EN_JMS_GADGETS_REGISTRY', "Registry Keys");
@@ -93,9 +62,33 @@ define('_EN_JMS_GADGETS_UPDATED_NO_NEED', "There is no need to update the {0} ga
 define('_EN_JMS_GADGETS_REQUIRES_X_GADGET', "{0} requires these gadgets to be installed to work: {1}");
 define('_EN_JMS_GADGETS_REQUIRES_X_DEPENDENCY', "{0} can't be {2} because {1} gadget(s) depends on it");
 
-/* Sidebar */
-define('_EN_JMS_SIDEBAR_ENABLE', "Enable");
-define('_EN_JMS_SIDEBAR_DISABLE', "Disable");
+/* Plugins */
+define('_EN_JMS_PLUGINS', "Plugins");
+define('_EN_JMS_PLUGINS_PLUGIN', "Plugin");
+define('_EN_JMS_PLUGINS_INSTALLED', "Installed plugins");
+define('_EN_JMS_PLUGINS_INSTALLED_DESC', "There are plugins you can uninstall.");
+define('_EN_JMS_PLUGINS_NOTINSTALLED', "Not installed plugins");
+define('_EN_JMS_PLUGINS_NOTINSTALLED_DESC', "There are plugins waiting to be installed.");
+
+define('_EN_JMS_PLUGINS_USAGE', "Plugin Usage");
+define('_EN_JMS_PLUGINS_NO_EXAMPLE', "No example found");
+define('_EN_JMS_PLUGINS_FRIENDLY', "Friendly");
+define('_EN_JMS_PLUGINS_NOT_FRIENDLY', "Not friendly");
+define('_EN_JMS_PLUGINS_ACCESSKEY', "Access Key");
+define('_EN_JMS_PLUGINS_NO_ACCESSKEY', "No access key found");
+define('_EN_JMS_PLUGINS_USE_IN', "Plugin usage");
+define('_EN_JMS_PLUGINS_SAVED', "Your plugin settings have been saved.");
+define('_EN_JMS_PLUGINS_NOTHING', "No plugins");
+define('_EN_JMS_PLUGINS_ALWAYS', "Always");
+define('_EN_JMS_PLUGINS_ONLY_IN_GADGET', "Only in these pluggable gadgets");
+define('_EN_JMS_PLUGINS_ENABLED_OK', "The {0} plugin has been installed sucessfully");
+define('_EN_JMS_PLUGINS_ENABLED_FAILURE', "There was a problem installing {0} plugin");
+define('_EN_JMS_PLUGINS_NO_SELECTION', "No plugin has been selected, please select one");
+define('_EN_JMS_PLUGINS_DISABLE_FAILURE', "Could not disable plugin");
+define('_EN_JMS_PLUGINS_DISABLE_OK', "The {0} plugin has been uninstalled sucessfully");
+define('_EN_JMS_PLUGINS_PLUGIN_DOESNT_EXISTS', "The {0} plugin does not exists");
+define('_EN_JMS_PLUGINS_PLUGIN_CANT_HAVE_NAME_CORE', "The plugin can't have the name Core, it's reserved for Jaws.");
+define('_EN_JMS_PLUGINS_CONFIRM_UNINSTALL', "Do you want to uninstall this plugin?");
 
 /* For compatability */
 define('_EN_JMS_SIDEBAR_DISABLED_GADGETS', "Disabled Gadgets");
@@ -104,10 +97,3 @@ define('_EN_JMS_SIDEBAR_ENABLED_GADGETS', "Enabled Gadgets");
 define('_EN_JMS_SIDEBAR_DISABLE_GADGETS_WAITING', "There are gadgets you can disable:");
 define('_EN_JMS_SIDEBAR_NOTUPDATED_GADGETS', "Outdated Gadgets");
 define('_EN_JMS_SIDEBAR_NOTUPDATED_SUGESTION', "These are gadgets that can be updated now:");
-
-/* Gadget disabling errors */
-define('_EN_JMS_SIDEBAR_DISABLE_MAIN_FAILURE', "Cannot disable main gadget");
-
-/* Plugins Sidebar */
-define('_EN_JMS_SIDEBAR_DISABLED_PLUGINS', "Disabled Plugins");
-define('_EN_JMS_SIDEBAR_PLUGINS_WAITING', "There are plugins waiting to be enabled:");
