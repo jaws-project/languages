7a8a9b0ef7afd2ff8379c93eb310b604e0e0d6ff
Mojtaba Ebrahimi <irangame@gmail.com>
2013-03-09 00:20:13 +0330

    [Comments]: Added new word

--- en/gadgets/Comments.php
+++ en/gadgets/Comments.php
@@ -11,11 +11,14 @@
  */
 
 define('_EN_COMMENTS_NAME', "Comments");
-define('_EN_COMMENTS_DESCRIPTION', "A gadget to mangament comments");
+define('_EN_COMMENTS_DESCRIPTION', "A gadget to management comments");
 
+define('_EN_COMMENTS_ACTIONS_COMMENTS', "Comments");
+define('_EN_COMMENTS_ACTIONS_COMMENTS_DESC', "show list of comments");
 define('_EN_COMMENTS_ACTIONS_RECENTCOMMENTS', "Recent comments");
 define('_EN_COMMENTS_ACTIONS_RECENTCOMMENTS_DESC', "show list of recent comments");
 
+define('_EN_COMMENTS_COMMENTS_PER_PAGE', "Comments per page");
 define('_EN_COMMENTS_RECENT_COMMENTS', "{0} recent comments");
 define('_EN_COMMENTS_GADGETS', "Gadget");
 define('_EN_COMMENTS_FILTER', "Filter");
@@ -23,6 +26,9 @@ define('_EN_COMMENTS_EDIT_MESSAGE_DETAILS', "Edit comment");
 define('_EN_COMMENTS_MESSAGE', "Message");
 define('_EN_COMMENTS_CONFIRM_DELETE', "Are you sure you want to delete this comment?");
 define('_EN_COMMENTS_REPLY', "Reply");
+define('_EN_COMMENTS_SEND', "Send Message");
+define('_EN_COMMENTS_COMMENTS', "Comments");
+define('_EN_COMMENTS_COMMENTS_COUNT', "{0} comments");
 
 define('_EN_COMMENTS_SEARCH_POST_ID_IS', "post ID is");
 define('_EN_COMMENTS_SEARCH_COMMENT_CONTAINS', "comment contains");
