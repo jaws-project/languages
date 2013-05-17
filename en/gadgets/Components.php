dab542c3879ea133294ac7f98724d536cf35e423
Mohsen Khahani <mkhahani@gmail.com>
2013-05-17 19:49:06 +0430

    [Components]: remove extra translations

--- en/gadgets/Components.php
+++ en/gadgets/Components.php
@@ -2,15 +2,15 @@
 /**
  * Meta Data
  *
- * "Project-Id-Version: Jms"
- * "Last-Translator: Helgi Þormar Þorbjörnsson"
+ * "Project-Id-Version: Components"
+ * "Last-Translator: Mohsen Khahani <mkhahani@gmail.com>"
  * "Language-Team: EN"
  * "MIME-Version: 1.0"
  * "Content-Type: text/plain; charset=UTF-8"
  * "Content-Transfer-Encoding: 8bit"
  */
 
-/* JMS */
+/* Components */
 define('_EN_COMPONENTS_NAME', "Components");
 define('_EN_COMPONENTS_DESCRIPTION', "Manage gadgets and plugins");
 
@@ -52,26 +52,20 @@ define('_EN_COMPONENTS_GADGETS_TOTAL', "Total gadgets");
 define('_EN_COMPONENTS_GADGETS_CONFIRM_UNINSTALL', "Do you want to uninstall the gadget? This will delete data from database.");
 define('_EN_COMPONENTS_GADGETS_CONFIRM_DISABLE', "Do you want to disable the gadget? This does not delete data from database.");
 define('_EN_COMPONENTS_GADGETS_NOT_EXISTS', "The gadget does not exists.");
-define('_EN_COMPONENTS_GADGETS_DISABLE_MAIN_FAILURE', "Cannot disable main gadget.");
 define('_EN_COMPONENTS_GADGETS_ENABLE_OK', "The {0} gadget has been enabled sucessfully.");
 define('_EN_COMPONENTS_GADGETS_ENABLE_FAILURE', "There was a problem installing {0} gadget.");
 define('_EN_COMPONENTS_GADGETS_DISABLE_OK', "The {0} gadget has been disabled successfully.");
-define('_EN_COMPONENTS_GADGETS_DISABLE_FAILURE', "There was a problem uninstalling {0} gadget.");
 define('_EN_COMPONENTS_GADGETS_UPDATE_OK', "The {0} gadget has been updated successfully.");
-define('_EN_COMPONENTS_GADGETS_UPDATE_FAILURE', "There was a problem updating {0} gadget.");
 define('_EN_COMPONENTS_GADGETS_UPDATE_NO_NEED', "There is no need to update the {0} gadget.");
-define('_EN_COMPONENTS_GADGETS_REQUIRES_X_GADGET', "{0} requires these gadgets to be installed to work: {1}");
-define('_EN_COMPONENTS_GADGETS_REQUIRES_X_DEPENDENCY', "{0} can't be {2} because {1} gadget(s) depends on it.");
 
 /* Plugins */
 define('_EN_COMPONENTS_PLUGINS', "Plugins");
-define('_EN_COMPONENTS_PLUGINS_PLUGIN', "Plugin");
 define('_EN_COMPONENTS_PLUGINS_INSTALLED', "Installed plugins");
 define('_EN_COMPONENTS_PLUGINS_INSTALLED_DESC', "There are plugins you can uninstall");
 define('_EN_COMPONENTS_PLUGINS_NOTINSTALLED', "Not installed plugins");
 define('_EN_COMPONENTS_PLUGINS_NOTINSTALLED_DESC', "There are plugins waiting to be installed");
 define('_EN_COMPONENTS_PLUGINS_USAGE', "Usage");
-define('_EN_COMPONENTS_PLUGINS_USAGE_DESC', "Select gadgets you want to use with this plugin:");
+define('_EN_COMPONENTS_PLUGINS_USAGE_DESC', "Select gadgets you want to be used with this plugin:");
 define('_EN_COMPONENTS_PLUGINS_USAGE_GADGET', "Gadget");
 define('_EN_COMPONENTS_PLUGINS_USAGE_BACKEND', "Back-end");
 define('_EN_COMPONENTS_PLUGINS_USAGE_FRONTEND', "Front-end");
@@ -80,7 +74,6 @@ define('_EN_COMPONENTS_PLUGINS_FRIENDLY', "Friendly");
 define('_EN_COMPONENTS_PLUGINS_NOT_FRIENDLY', "Not friendly");
 define('_EN_COMPONENTS_PLUGINS_ACCESSKEY', "Access Key");
 define('_EN_COMPONENTS_PLUGINS_NO_ACCESSKEY', "No access key found");
-define('_EN_COMPONENTS_PLUGINS_USE_ALWAYS', "All Gadgets");
 define('_EN_COMPONENTS_PLUGINS_TOTAL', "Total plugins");
 define('_EN_COMPONENTS_PLUGINS_UPDATED', "Your plugin settings have been updated.");
 define('_EN_COMPONENTS_PLUGINS_INSTALL_OK', "The {0} plugin has been installed sucessfully.");
@@ -88,13 +81,12 @@ define('_EN_COMPONENTS_PLUGINS_INSTALL_FAILURE', "There was a problem installing
 define('_EN_COMPONENTS_PLUGINS_CONFIRM_UNINSTALL', "Do you want to uninstall this plugin?");
 define('_EN_COMPONENTS_PLUGINS_UNINSTALL_OK', "The {0} plugin has been uninstalled sucessfully.");
 define('_EN_COMPONENTS_PLUGINS_UNINSTALL_FAILURE', "Could not disable plugin");
-define('_EN_COMPONENTS_PLUGINS_PLUGIN_DOESNT_EXISTS', "The {0} plugin does not exists.");
 define('_EN_COMPONENTS_PLUGINS_PLUGIN_CANT_HAVE_NAME_CORE', "The plugin can't have the name Core, it's reserved for Jaws.");
 
 /* Registry */
 define('_EN_COMPONENTS_REGISTRY_UPDATED', "Registery changes have been updated.");
 define('_EN_COMPONENTS_REGISTRY_NOT_UPDATED', "Error while updating registery changes.");
 
-/* ACL */
+/* ACLs */
 define('_EN_COMPONENTS_ACL_UPDATED', "ACL changes have been updated.");
 define('_EN_COMPONENTS_ACL_NOT_UPDATED', "Error while updating ACL changes.");
