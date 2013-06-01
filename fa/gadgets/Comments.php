0850b82672d50306b801db6d5225953dffde6c7e
Mojtaba Ebrahimi <irangame@gmail.com>
2013-06-02 01:06:10 +0430

    [Comments]: Added Persian translation

---
+++ fa/gadgets/Comments.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: Comments"
+ * "Last-Translator: Mojtaba Ebrahimi <ebrahimi@zehneziba.ir>"
+ * "Language-Team: FA"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+
+define('_FA_COMMENTS_ACL_DEFAULT', "مدیریت نظرات");
+define('_FA_COMMENTS_ACL_MANAGECOMMENTS', "مدیریت نظرات مربوط به تمامی ابزارها");
+define('_FA_COMMENTS_ACL_REPLYCOMMENTS', "پاسخ به نظرات");
+define('_FA_COMMENTS_ACL_SETTINGS', "ویرایش تنظیمات");
+define('_FA_COMMENTS_NAME', "نظرات");
+define('_FA_COMMENTS_DESCRIPTION', "ابزاری جهت مدیریت نظرات وب سایت");
+define('_FA_COMMENTS_ACTIONS_COMMENTS', "نظرات");
+define('_FA_COMMENTS_ACTIONS_COMMENTS_DESC', "نمایش لیست نظرات");
+define('_FA_COMMENTS_ACTIONS_RECENTCOMMENTS', "نظرات اخیر");
+define('_FA_COMMENTS_ACTIONS_RECENTCOMMENTS_DESC', "نمایش فهرست نظرات اخیر");
+define('_FA_COMMENTS_COMMENTS_PER_PAGE', "تعداد نظرات در هر صفحه");
+define('_FA_COMMENTS_RECENT_COMMENTS', "{0} نظر اخیر");
+define('_FA_COMMENTS_GADGETS', "ابزار");
+define('_FA_COMMENTS_FILTER', "جستجو");
+define('_FA_COMMENTS_EDIT_MESSAGE_DETAILS', "ویرایش نظر");
+define('_FA_COMMENTS_MESSAGE', "متن پیام");
+define('_FA_COMMENTS_CONFIRM_DELETE', "آیا قصد دارید این نظر را حذف کنید؟");
+define('_FA_COMMENTS_REPLY', "پاسخ");
+define('_FA_COMMENTS_SEND', "ارسال پیام");
+define('_FA_COMMENTS_COMMENTS', "نظرات");
+define('_FA_COMMENTS_COMMENTS_COUNT', "{0} نظر");
+define('_FA_COMMENTS_USERS_REGISTERED_DATE', "تاریخ عضویت");
+define('_FA_COMMENTS_POSTEDBY', "ارسال شده توسط");
+define('_FA_COMMENTS_REPLY_TO_COMMENT', "پاسخ");
+define('_FA_COMMENTS_REPLIER', "پاسخ به");
+define('_FA_COMMENTS_ANTISPAM_ALLOWDUPLICATE', "اجازه ارسال پیام های تکراری");
+define('_FA_COMMENTS_PROPERTIES_UPDATED', "تنظیمات مورد نظر ثبت شد");
+define('_FA_COMMENTS_ERROR_CANT_UPDATE_PROPERTIES', "خطا در ثبت تنظیمات");
+define('_FA_COMMENTS_ALLOW_COMMENTS', "فعال بودن نظرات در کل وب سایت");
+define('_FA_COMMENTS_ALLOW_COMMENTS_RESTRICTED', "محدود شده");
+define('_FA_COMMENTS_COMMENTS_STATUS', "وضعیت نظر");
+define('_FA_COMMENTS_STATUS_APPROVED', "تایید شده");
+define('_FA_COMMENTS_STATUS_WAITING', "منتظر تایبد");
+define('_FA_COMMENTS_STATUS_SPAM', "اسپم");
+define('_FA_COMMENTS_MARK_AS_APPROVED', "مهر تایید");
+define('_FA_COMMENTS_MARK_AS_WAITING', "مهر انتظار");
+define('_FA_COMMENTS_MARK_AS_SPAM', "مهر اسپم");
+define('_FA_COMMENTS_SPAM_POSSIBLE_DUPLICATE_MESSAGE', "اجازه درج پیام های تکراری وجود ندارد");
+define('_FA_COMMENTS_MESSAGE_SENT', "پیام شما با موفقیت ارسال شد");
+define('_FA_COMMENTS_COMMENTS_RESTRICTED', "حهت ارسال پيام بايد <a href=\"{0}\">ورود</a> or <a href=\"{1}\">ثبت نام</a> کنيد");
+define('_FA_COMMENTS_COMMENT_UPDATED', "نظر مورد نظر ویرایش شد");
+define('_FA_COMMENTS_COMMENT_MARKED', "وضعیت نظر مورد نظر ویرایش شد");
+define('_FA_COMMENTS_COMMENT_DELETED', "نظر مورد نظر حذف شد");
