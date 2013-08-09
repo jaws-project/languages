de98766d1d9a795dcdf7db3a8814c475979d5d0b
Ali Fazelzadeh <afz@php.net>
2013-08-09 13:38:03 +0430

    [FastLinks]: update example statement

--- en/plugins/FastLinks.php
+++ en/plugins/FastLinks.php
@@ -13,7 +13,7 @@
 
 //FastLinks translation - en
 define('_EN_PLUGINS_FASTLINKS_DESCRIPTION', "Replaces [email] and [url] tags to their respective XHTML tags");
-define('_EN_PLUGINS_FASTLINKS_EXAMPLE', "");
+define('_EN_PLUGINS_FASTLINKS_EXAMPLE', "[email=\"user@jaws.com.mx\"]MyFriend[/email]<br />[email]user@jaws.com.mx[/email]<br />[url]http://www.jaws-project.com[/url]<br />[url=\"http://www.jaws-project.com\"]Jaws Site[/url]");
 define('_EN_PLUGINS_FASTLINKS_ADD_SITE',  "Add a link to a site");
 define('_EN_PLUGINS_FASTLINKS_ADD_EMAIL', "Add a link to an email");
 define('_EN_PLUGINS_FASTLINKS_YOUREMAIL', "Your e-mail");
