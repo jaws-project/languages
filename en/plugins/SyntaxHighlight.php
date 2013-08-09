30b6394ab189aa5d5ed5d5ff1f0797d156449cb6
Ali Fazelzadeh <afz@php.net>
2013-08-09 14:06:35 +0430

    [SyntaxHighlight]: update example statement

--- en/plugins/SyntaxHighlight.php
+++ en/plugins/SyntaxHighlight.php
@@ -13,7 +13,7 @@
 
 //SyntaxHighlight Translation - en
 define('_EN_PLUGINS_SYNTAXHIGHLIGHT_DESCRIPTION', "Highlight your code with nice colors");
-define('_EN_PLUGINS_SYNTAXHIGHLIGHT_EXAMPLE', "");
+define('_EN_PLUGINS_SYNTAXHIGHLIGHT_EXAMPLE', "[code=\"C#\"]<br />using System;<br />public class Foo { }<br />[/code]");
 define('_EN_PLUGINS_SYNTAXHIGHLIGHT_YOUR_CODE', "Your Code");
 define('_EN_PLUGINS_SYNTAXHIGHLIGHT_ADD', "Add code to your text");
 define('_EN_PLUGINS_SYNTAXHIGHLIGHT_ADD_TERMINAL', "Apply a Terminal look and feel to your code");
