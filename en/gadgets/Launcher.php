426fd63b50e8cfc541e8718e4d4f4d9513f3e092
Ali Fazelzadeh <afz@php.net>
2013-08-15 10:46:58 +0430

    [Launcher]: Added missed statement

--- en/gadgets/Launcher.php
+++ en/gadgets/Launcher.php
@@ -16,9 +16,10 @@ define('_EN_LAUNCHER_NAME', "Launcher");
 define('_EN_LAUNCHER_DESCRIPTION', "A minimalistic gadget to run independent scripts");
 
 /* Strings */
-define ('_EN_LAUNCHER_ACTIONS_EXECUTE', "Script");
-define ('_EN_LAUNCHER_ACTIONS_EXECUTE_DESC', "Execute this script");
+define('_EN_LAUNCHER_SCRIPT', "Script");
+define('_EN_LAUNCHER_ACTIONS_EXECUTE', "Script");
+define('_EN_LAUNCHER_ACTIONS_EXECUTE_DESC', "Execute this script");
 
 /* Errors */
-define ('_EN_LAUNCHER_ERROR_SCRIPT_NOT_EXISTS', "Script doesn't exists");
-define ('_EN_LAUNCHER_ERROR_FUNCTION_NOT_EXISTS', "Function doesn't exists");
+define('_EN_LAUNCHER_ERROR_SCRIPT_NOT_EXISTS', "Script doesn't exists");
+define('_EN_LAUNCHER_ERROR_FUNCTION_NOT_EXISTS', "Function doesn't exists");
--- fa/gadgets/Launcher.php
+++ fa/gadgets/Launcher.php
@@ -12,6 +12,7 @@
 
 define('_FA_LAUNCHER_NAME', "سکو");
 define('_FA_LAUNCHER_DESCRIPTION', "ابزاری برای اجرای اسکریپت های مستقل");
+define('_FA_LAUNCHER_SCRIPT', "اسکریپت");
 define('_FA_LAUNCHER_ACTIONS_EXECUTE', "اسکریپت");
 define('_FA_LAUNCHER_ACTIONS_EXECUTE_DESC', "اجرای این اسکریپت");
 define('_FA_LAUNCHER_ERROR_SCRIPT_NOT_EXISTS', "اسکریپت دلخواه شما پیدا نشد");
