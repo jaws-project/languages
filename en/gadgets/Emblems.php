aca6d8731ce2c37ce759a78e3fdd270d1b031617
Mohsen Khahani <mkhahani@gmail.com>
2013-08-25 11:07:51 +0430

    [Emblems]: update en & fa translations

--- en/gadgets/Emblems.php
+++ en/gadgets/Emblems.php
@@ -12,14 +12,6 @@
 
 define('_EN_EMBLEMS_NAME', "Emblems");
 define('_EN_EMBLEMS_DESCRIPTION', "A gadget to display emblems in your site");
-define('_EN_EMBLEMS_ADMIN_MESSAGE', "Here you can update the emblems properties and add/update/delete emblems");
-
-// ACL's
-define('_EN_EMBLEMS_ACL_ADDEMBLEM', "Add Emblem");
-define('_EN_EMBLEMS_ACL_DEFAULT', "Manage Emblems");
-define('_EN_EMBLEMS_ACL_DELETEEMBLEM', "Delete Emblems");
-define('_EN_EMBLEMS_ACL_EDITEMBLEM', "Edit Emblems");
-define('_EN_EMBLEMS_ACL_UPDATEPROPERTIES', "Update Emblems Gadget Properties");
 
 // Actions
 define('_EN_EMBLEMS_ACTIONS_DISPLAY', "Display");
@@ -28,28 +20,20 @@ define('_EN_EMBLEMS_ACTION_TITLE', "Emblems");
 define('_EN_EMBLEMS_ACTION_DEFAULT', "Default Action");
 
 //Strings
-define('_EN_EMBLEMS_ACTIVE', "Active");
-define('_EN_EMBLEMS_INACTIVE', "Inactive");
 define('_EN_EMBLEMS_ADD_EMBLEM', "Add a new emblem");
-define('_EN_EMBLEMS_SETTINGS', "Emblems General Settings");
-define('_EN_EMBLEMS_ALLOW_URL', "Display URL in emblems?");
-define('_EN_EMBLEMS_ROWS_LIMIT', "Limit to rows: ");
-define('_EN_EMBLEMS_LIST', "Emblem List");
-define('_EN_EMBLEMS_SRC', "Image Source");
+define('_EN_EMBLEMS_RESULT', "Result");
 define('_EN_EMBLEMS_TYPE', "Type");
-define('_EN_EMBLEMS_STATUS', "Status");
-define('_EN_EMBLEMS_BEST_VIEW', "Best Viewed With ");
-define('_EN_EMBLEMS_IS_VALID', "Is Valid ");
-define('_EN_EMBLEMS_LICENSED_UNDER', "Licensed Under ");
-define('_EN_EMBLEMS_POWERED_BY', "Powered By ");
-define('_EN_EMBLEMS_SUPPORTS', "Supports ");
+define('_EN_EMBLEMS_TYPE_1', "Powered By ");
+define('_EN_EMBLEMS_TYPE_2', "Supports ");
+define('_EN_EMBLEMS_TYPE_3', "Is Valid ");
+define('_EN_EMBLEMS_TYPE_4', "Licensed Under ");
+define('_EN_EMBLEMS_TYPE_5', "Best Viewed With ");
 
 //Messages
 define('_EN_EMBLEMS_ADDED', "The emblem was added to the database");
 define('_EN_EMBLEMS_CONFIRM_DELETE', "Are you sure you want to delete this emblem?");
 define('_EN_EMBLEMS_DELETED', "The emblem was deleted");
+define('_EN_EMBLEMS_ERROR_NO_IMAGE_UPLOADED', "No image has been uploaded.");
 define('_EN_EMBLEMS_ERROR_NOT_ADDED', "The emblem was not added");
-define('_EN_EMBLEMS_ERROR_PROPERTIES_NOT_UPDATED', "The properties were not updated");
 define('_EN_EMBLEMS_FILE_NOT_DELETED', "The emblem file wasn't deleted, you'll have to delete it manually");
-define('_EN_EMBLEMS_PROPERTIES_UPDATED', "The properties were updated successfully");
 define('_EN_EMBLEMS_UPDATED', "The emblem {0} got updated");
