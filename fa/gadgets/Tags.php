41142a3f5f3a24fb78d0bdf32da8d8ba241dcd64
Mojtaba Ebrahimi <irangame@gmail.com>
2013-10-10 19:20:45 +0330

    [Tags]: added fa translation

---
+++ fa/gadgets/Tags.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Meta Data
+ *
+ * "Project-Id-Version: Tags"
+ * "Last-Translator: "
+ * "Language-Team: FA"
+ * "MIME-Version: 1.0"
+ * "Content-Type: text/plain; charset=UTF-8"
+ * "Content-Transfer-Encoding: 8bit"
+ */
+define('_FA_TAGS_NAME', "برچسبها");
+define('_FA_TAGS_DESCRIPTION', "ابزاری جهت مدیریت برچسبها");
+define('_FA_TAGS_ACL_DEFAULT', "مدیریت برچسبها");
+define('_FA_TAGS_ACL_MANAGEPROPERTIES', "مدیریت خصوصیات");
+define('_FA_TAGS_ACL_ADDTAGS', "افزودن برچسب");
+define('_FA_TAGS_ACL_DELETETAGS', "زدودن برچسبها");
+define('_FA_TAGS_ACL_MERGETAGS', "در هم آمیختن");
+define('_FA_TAGS_ACTIONS_TAGCLOUD', "ابر برچسب");
+define('_FA_TAGS_ACTIONS_TAGCLOUD_DESC', "نمایش ابر برچسب");
+define('_FA_TAGS_TAG_CLOUD', "ابر برچسب {0}");
+define('_FA_TAGS_VIEW_TAG', "نمایش برچسب {0}");
+define('_FA_TAGS_TAG_ITEM_COUNT', "{0} نوشته");
+define('_FA_TAGS_TAG_NAME', "نام برچسب");
+define('_FA_TAGS_TAG_USAGE_COUNT', "تعداد استفاده شده");
+define('_FA_TAGS_ADD_TAG', "افزودن برچسب");
+define('_FA_TAGS_EDIT_TAG', "ویرایش برچسب");
+define('_FA_TAGS_CONFIRM_DELETE', "آیا می خواهید این برچسب(ها) را پاک کنید؟");
+define('_FA_TAGS_SELECT_MORE_THAN_ONE_TAG_FOR_MERGE', "برای در هم آمیختن برچسب ها باید بیش از یک برچسب را برگزینید");
+define('_FA_TAGS_GADGET', "ابزار");
+define('_FA_TAGS_MERGE', "در هم آمیختن");
+define('_FA_TAGS_TAG_ADDED', "برچسب افزوده شد");
+define('_FA_TAGS_TAG_UPDATED', "برچسب ویرایش شد");
+define('_FA_TAGS_TAGS_MERGED', "برچسب ها در هم آمیختند");
+define('_FA_TAGS_TAG_DELETED', "برچسب حذف شد");
